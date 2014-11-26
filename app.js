9ff6c72950373ec50ae1170eb89e5ac4620fe06c
Jose Ignacio <joserobleda@gmail.com>
2014-11-26 21:55:34 +0100

    allow CORS

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-require('neasy').start();
+var app = require('neasy')
+
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	if ('OPTIONS' === req.method) {
+		return res.send(200);
+    }
+
+	next();
+});
+
+app.start();
